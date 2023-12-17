6a5cfb3fea979331ee565f9919e5df30b430c47d
EgorG <61026111+egor-LT@users.noreply.github.com>
2023-12-17 20:57:25 +0300

    Add files via upload

---
+++ Lb5/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lb5
+{
+    public abstract class Vehicle
+    {
+        public int Speed { get; set; }
+        public int Capacity { get; set; }
+
+        public abstract void Move();
+    }
+
+    
+    public class Human
+    {
+        public int Speed { get; set; }
+
+        public void Move()
+        {
+            
+        }
+    }
+
+    
+    public class Car : Vehicle
+    {
+        
+        public override void Move()
+        {
+            
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        
+        public override void Move()
+        {
+           
+        }
+    }
+
+    public class Train : Vehicle
+    {
+      
+        public override void Move()
+        {
+            
+        }
+    }
+
+    
+    public class Route
+    {
+        public string StartPoint { get; set; }
+        public string EndPoint { get; set; }
+
+        
+        public void CalculateOptimalRoute(Car car)
+        {
+            
+        }
+
+        public void CalculateOptimalRoute(Bus bus)
+        {
+           
+        }
+
+        public void CalculateOptimalRoute(Train train)
+        {
+            
+        }
+    }
+
+    
+    public class TransportNetwork
+    {
+        private List<Vehicle> vehicles;
+
+        public TransportNetwork()
+        {
+            vehicles = new List<Vehicle>();
+        }
+
+        
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        
+        public void ControlMovement()
+        {
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Move();
+            }
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Speed = 60;
+            car.Capacity = 5;
+
+            Bus bus = new Bus();
+            bus.Speed = 40;
+            bus.Capacity = 30;
+
+            Train train = new Train();
+            train.Speed = 100;
+            train.Capacity = 200;
+
+            TransportNetwork network = new TransportNetwork();
+            network.AddVehicle(car);
+            network.AddVehicle(bus);
+            network.AddVehicle(train);
+
+            network.ControlMovement();
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class MathOperations
+    {
+        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+      
+        public static int[] Add(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+            {
+                throw new ArgumentException("Arrays must have the same length.");
+            }
+
+            int[] result = new int[arr1.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = arr1[i] + arr2[i];
+            }
+            return result;
+        }
+
+       
+        public static int[,] Add(int[,] matrix1, int[,] matrix2)
+        {
+            int rows = matrix1.GetLength(0);
+            int cols = matrix1.GetLength(1);
+
+            if (rows != matrix2.GetLength(0) || cols != matrix2.GetLength(1))
+            {
+                throw new ArgumentException("Matrices must have the same dimensions.");
+            }
+
+            int[,] result = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return result;
+        }
+
+
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int[] Subtract(int[] arr1, int[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+            {
+                throw new ArgumentException("Arrays must have the same length.");
+            }
+
+            int[] result = new int[arr1.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = arr1[i] - arr2[i];
+            }
+            return result;
+        }
+
+       
+
+        
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int result1 = MathOperations.Add(5, 3);
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = { 4, 5, 6 };
+            int[] result2 = MathOperations.Add(array1, array2);
+
+            int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+            int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+            int[,] result3 = MathOperations.Add(matrix1, matrix2);
+
+            
+            Console.WriteLine("Result 1: " + result1);
+            Console.WriteLine("Result 2: [" + string.Join(", ", result2) + "]");
+            Console.WriteLine("Result 3:");
+            for (int i = 0; i < result3.GetLength(0); i++)
+            {
+                for (int j = 0; j < result3.GetLength(1); j++)
+                {
+                    Console.Write(result3[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
